8c9f4d61a48f9cb8bf55b4d5e57c0314cae5c116
Elicarlos <elicarlosantos_@hotmail.com>
2020-11-17 02:11:57 -0300

    ajuste de model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Tamanho(models.Model):
+    tamanho = models.CharField(max_length=30)
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+
+class Produto(models.Model):
+    descricao = models.CharField(max_length=50)
+    preco = models.DecimalField()
+    estoque = models.IntegerField()
+    foto = models.ImageField(upload_to='produtos')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    tamanho = models.ForeignKey(Tamanho, on_delete=models.CASCADE)
+    genero = models.CharField(max_length=30)
+    ativo = models.BooleanField(default=True)
+
+
+class Cliente():
+    cpf = models.CharField(max_length=30)
+    nome = models.CharField(models.Model)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+
+
+
+
