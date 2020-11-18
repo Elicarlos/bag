1f56ca3fbdff96854d93ec9eb045d39985db9548
Elicarlos <elicarlosantos_@hotmail.com>
2020-11-18 04:24:48 -0300

    Ajuste de models

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import a
+from .models import Cliente, Categoria, Produto, Pedido, Tamanho
+
+admin.site.register(Pedido)
+
 
 # Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['cpf', 'nome']
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['categoria']
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['descricao']
+
+
+
+
+@admin.register(Tamanho)
+class TamanhoAdmin(admin.ModelAdmin):
+    list_display = ['tamanho']
--- core/models.py
+++ core/models.py
@@ -4,13 +4,21 @@ from django.db import models
 
 class Tamanho(models.Model):
     tamanho = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.tamanho
+
 
 class Categoria(models.Model):
     categoria = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.categoria
+
+
 class Produto(models.Model):
     descricao = models.CharField(max_length=50)
-    preco = models.DecimalField()
+    preco = models.FloatField()
     estoque = models.IntegerField()
     foto = models.ImageField(upload_to='produtos')
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
@@ -19,11 +27,35 @@ class Produto(models.Model):
     ativo = models.BooleanField(default=True)
 
 
-class Cliente():
+class Cliente(models.Model):
     cpf = models.CharField(max_length=30)
-    nome = models.CharField(models.Model)
+    nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.nome)
+
+    class Meta:
+        db_table = 'cliente'
+
+
+
+
+class Pedido(models.Model):  
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='pedido')
+    itens = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='pedido')
+    data = models.DateField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2) 
+          
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = 'pedido'
+
+
 
 
 
